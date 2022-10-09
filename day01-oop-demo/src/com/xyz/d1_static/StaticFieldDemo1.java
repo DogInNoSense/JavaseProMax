44b74b026d735f7fefd2d5bf93f007cb55cfcf69
xyz <xieyinzhangl@gmail.com>
2022-10-09 20:32:38 +0800

    first commit

---
+++ day01-oop-demo/src/com/xyz/d1_static/StaticFieldDemo1.java
@@ -0,0 +1,7 @@
+package com.xyz.d1_static;
+
+public class StaticFieldDemo1 {
+    public static void main(String[] args) {
+        // 理解static修饰成员变量的左右和访问特点
+    }
+}
---
+++ day01-oop-demo/src/com/xyz/d1_static/User.java
@@ -0,0 +1,8 @@
+package com.xyz.d1_static;
+
+public class User {
+    /*
+    在线人数
+     */
+    public static int onlineNumber;
+}

703243b1f8a3a04a72bf550513ddb80fece5ced9
xyz <xieyinzhangl@gmail.com>
2022-10-13 21:33:10 +0800

    first commit

---
+++ day02-oop-demo/day02-oop-demo/src/com/xyz/d6_abstract/Dog.java
@@ -0,0 +1,11 @@
+package com.xyz.d6_abstract;
+
+public class Dog extends Animal {
+
+
+    @Override
+    public void run() {
+        System.out.println("狗跑的贼快!");
+    }
+
+}
---
+++ day02-oop-demo/day02-oop-demo/src/com/xyz/d6_abstract/Test.java
@@ -0,0 +1,10 @@
+package com.xyz.d6_abstract;
+
+public class Test {
+    public static void main(String[] args) {
+        // 目标: 先认识抽象类,再了解使用场景
+        Dog d = new Dog();
+        d.run();
+
+    }
+}

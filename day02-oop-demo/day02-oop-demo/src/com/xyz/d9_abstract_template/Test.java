135fbf32b62152fafa3b6027ddee74712f824318
xyz <xieyinzhangl@gmail.com>
2022-10-13 22:07:18 +0800

    first commit

---
+++ day02-oop-demo/day02-oop-demo/src/com/xyz/d9_abstract_template/Test.java
@@ -0,0 +1,10 @@
+package com.xyz.d9_abstract_template;
+
+public class Test {
+    public static void main(String[] args) {
+        // 模板方法模式的思想和使用步骤
+        // 提高代码复用性
+        StudentMiddle s = new StudentMiddle();
+        s.write();
+    }
+}

ad970fa64806359b9c3a055d210527b72aecdcdd
xyz <xieyinzhangl@gmail.com>
2022-10-14 20:07:07 +0800

    first commit

---
+++ day02-oop-demo/day02-oop-demo/src/com/xyz/d12_interface_extends/BasketBallMan.java
@@ -0,0 +1,38 @@
+package com.xyz.d12_interface_extends;
+
+import com.xyz.d11_interface_implements.Law;
+
+public class BasketBallMan {
+    /*
+    一个实现类的
+     */
+    public class BasketballMan implements SportMan {
+
+
+        @Override
+        public void rule() {
+
+        }
+
+        @Override
+        public void eat() {
+
+        }
+
+        @Override
+        public void sleep() {
+
+        }
+
+        @Override
+        public void run() {
+
+        }
+
+        @Override
+        public void competition() {
+
+        }
+    }
+}
+

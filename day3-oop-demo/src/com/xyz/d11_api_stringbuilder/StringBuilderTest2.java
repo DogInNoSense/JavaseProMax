87b194c3ef171ea9011b6060e4b9cc83287a14ba
xyz <xieyinzhangl@gmail.com>
2022-10-17 22:14:19 +0800

    first commit

--- day3-oop-demo/src/com/xyz/d11_api_stringbuilder/StringBuilderTest2.java
+++ day3-oop-demo/src/com/xyz/d11_api_stringbuilder/StringBuilderTest2.java
@@ -1,4 +1,32 @@
 package com.xyz.d11_api_stringbuilder;
 
 public class StringBuilderTest2 {
+    public static void main(String[] args) {
+        int arr1[] = null;
+        System.out.println(toString(arr1));
+
+
+        int arr2[] = {10, 11, 13, 14};
+        System.out.println(toString(arr2));
+
+        int arr3[] = {};
+        System.out.println(toString(arr3));
+    }
+
+    /*
+    1.定义方法接受任意整型数组,返回数组内容格式 (拼接字符串)
+     */
+    public static String toString(int arr[]) {
+        // 2.拼接内容
+        if (arr != null) {
+            StringBuilder sb = new StringBuilder("[");
+            for (int i = 0; i < arr.length; i++) {
+                sb.append(arr[i]).append(i == arr.length - 1 ? "" : ", ");
+            }
+            sb.append("]");
+            return sb.toString();
+        } else {
+            return "[]"; // null
+        }
+    }
 }

a62decaa4a9eb17f9a21e2d8d0b0db6e5b2b7d31
xyz <xieyinzhangl@gmail.com>
2022-10-18 19:51:02 +0800

    first commit

---
+++ day3-oop-demo/src/com/xyz/d12_math/MathDemo.java
@@ -0,0 +1,28 @@
+package com.xyz.d12_math;
+
+public class MathDemo {
+    public static void main(String[] args) {
+        // 1.取绝对值,返回正数
+        System.out.println(Math.abs(10)); // 10
+        System.out.println(Math.abs(-10.3)); // 10.3
+
+        // 2.向上取整:5
+        System.out.println(Math.ceil((4.000000000001))); // 5
+        System.out.println(Math.ceil((4.0))); // 4
+
+
+        // 3.向下取整:5
+        System.out.println(Math.floor(5.9999999)); // 5
+        System.out.println(Math.ceil((4.0))); // 4
+
+
+        // 4.求指数次方
+        System.out.println(Math.pow(2, 10)); // 2^10
+
+        // 5.四舍五入
+        System.out.println(Math.round(4.999));
+        System.out.println(Math.round(4.50001));
+
+        System.out.println(Math.random()); // 范围:0.0-1.0 左闭右开
+    }
+}

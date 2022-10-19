2d75f5a08f7700d57c8ed6e0a17331bf697e2943
xyz <xieyinzhangl@gmail.com>
2022-10-19 22:29:59 +0800

    first commit

---
+++ api-lambda-app/src/d3_calendar/CalendarDemo1.java
@@ -0,0 +1,30 @@
+package d3_calendar;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarDemo1 {
+    public static void main(String[] args) {
+        // 1.拿到系统此刻日历对象
+        Calendar cal = Calendar.getInstance();
+        System.out.println(cal);
+        // 2.获取日历信息
+        int year = cal.get(Calendar.YEAR);
+        System.out.println(year);
+        int month = cal.get(Calendar.MONTH) + 1;
+        System.out.println(month);
+        int days = cal.get(Calendar.DAY_OF_YEAR);
+        System.out.println(days);
+        // 3.修改日历的某个字段信息
+//        cal.set(Calendar.HOUR, 12);
+//        System.out.println(cal);
+
+        // 64天之后 是什么时间
+        cal.add(Calendar.DAY_OF_YEAR, 64);
+        cal.add(Calendar.MINUTE, 59);
+
+        // 拿到此刻日期对象
+        Date d = cal.getTime();
+        System.out.println(d);
+    }
+}

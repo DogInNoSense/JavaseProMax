e8092ddf5bcee8b8b340777e87379b4fb1ef40c6
xyz <xieyinzhangl@gmail.com>
2022-11-15 16:53:06 +0800

    first commit

---
+++ io-app2/src/com/xyz/d4_transfer_stream/CharSetTest00.java
@@ -0,0 +1,40 @@
+package com.xyz.d4_transfer_stream;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * 演示代码编码与文件编码相同和不同的情况
+ */
+public class CharSetTest00 {
+    public static void main(String[] args) {
+        try (
+                // 代码 : UTF-8 文件 UTF-8不会乱码
+                // 1.创建一个文件字符输入流与源文件接通
+                  Reader fr = new FileReader("io-app2/src/data7.txt");
+                // a.把低级的字符输入流包装成高级的缓冲字符输入流
+
+
+                // 代码 : UTF-8 文件 GBK会乱码
+                BufferedReader br = new BufferedReader(fr);
+
+        ) {
+            // 2.循环读取一个字符数组的数据
+//            char[] buffer = new char[1024];
+//            int len;
+//            while ((len = br.read(buffer)) != -1) {
+//                String rs = new String(buffer, 0, len);
+//                System.out.println(rs);
+//            }
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(br.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ io-app2/src/com/xyz/d4_transfer_stream/InputStreamReaderDemo1.java
@@ -0,0 +1,21 @@
+package com.xyz.d4_transfer_stream;
+
+import java.io.*;
+
+public class InputStreamReaderDemo1 {
+    public static void main(String[] args) throws Exception {
+        // 代码UTF-8 文件 GBK "io-app2/src/data7.txt"
+        // 1.提取GBK文件的原始字节流
+        InputStream is = new FileInputStream("io-app2/src/data7.txt");
+
+        // 2.把原始字节流转换成字符输入流
+
+//        Reader isr = new InputStreamReader(is); // 默认以UTF-8的方式转换成字符流,还是会乱码
+        Reader isr = new InputStreamReader(is, "GBK");
+        BufferedReader br = new BufferedReader(isr); // 以指定的GBK编码转换成字符流
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+}

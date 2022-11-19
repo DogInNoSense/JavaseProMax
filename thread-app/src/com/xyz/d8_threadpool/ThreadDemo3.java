f20be9ca51d8c27a65d387f12adcd8f3709e546f
xyz <xieyinzhangl@gmail.com>
2022-11-19 15:02:36 +0800

    first commit

---
+++ thread-app/src/com/xyz/d8_threadpool/ThreadDemo3.java
@@ -0,0 +1,21 @@
+package com.xyz.d8_threadpool;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 使用Executors直接得到一个线程对象
+ */
+public class ThreadDemo3 {
+    public static void main(String[] args) throws Exception {
+        // 1.创建固定线程数据的线程池
+        ExecutorService pool = Executors.newFixedThreadPool(3);
+
+        pool.execute(new MyRunnable());
+        pool.execute(new MyRunnable());
+        pool.execute(new MyRunnable());
+        pool.execute(new MyRunnable()); // 已经没有多余线程了
+
+    }
+}

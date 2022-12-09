86650050581fb7ae11d181e280617e81a26a6aa1
xyz <xieyinzhangl@gmail.com>
2022-12-09 19:49:37 +0800

    first commit

--- xml-app/src/com/xyz/d2_xpath/XPathDemo.java
+++ xml-app/src/com/xyz/d2_xpath/XPathDemo.java
@@ -1,4 +1,5 @@
-package com.itheima.d2_xpath;
+package com.xyz.d2_xpath;
+
 import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.Element;
@@ -9,38 +10,38 @@ import org.junit.Test;
 import java.util.List;
 
 /**
- 目标：XPath检索XML中的信息啊。(了解)
-
- 引入：
- Dom4J可以用于解析整个XML的数据。
- 但是如果要检索XML中的某些信息，建议使用XPath.（Xpath依赖Dom4j技术）
- Dom4J用于解析数据，Xpath用于检索数据。
- XPath使用步骤：
- 1.导入dom4j框架。（XPath依赖于Dom4j技术,必须先导入dom4j框架！）
- 2.导入XPath独有的框架包。jaxen-1.1.2.jar
- XPath常用API:
- List<Node> selectNodes(String var1):检索出一批节点集合。
- Node selectSingleNode(String var1)：检索出一个节点返回。
- XPath提供的四种检索数据的写法：
- 1.绝对路径。
- 2.相对路径。
- 3.全文搜索。
- 4.属性查找。
- 小结：
- 1.绝对路径： /根元素/子元素/子元素。
- 2.相对路径： ./子元素/子元素。 (.代表了当前元素)
- 3.全文搜索：
- //元素  在全文找这个元素
- //元素1/元素2  在全文找元素1下面的一级元素2
- //元素1//元素2  在全文找元素1下面的全部元素2
- 4.属性查找。
- //@属性名称  在全文检索属性对象。
- //元素[@属性名称]  在全文检索包含该属性的元素对象。
- //元素[@属性名称=值]  在全文检索包含该属性的元素且属性值为该值的元素对象。
+ * 目标：XPath检索XML中的信息啊。(了解)
+ * <p>
+ * 引入：
+ * Dom4J可以用于解析整个XML的数据。
+ * 但是如果要检索XML中的某些信息，建议使用XPath.（Xpath依赖Dom4j技术）
+ * Dom4J用于解析数据，Xpath用于检索数据。
+ * XPath使用步骤：
+ * 1.导入dom4j框架。（XPath依赖于Dom4j技术,必须先导入dom4j框架！）
+ * 2.导入XPath独有的框架包。jaxen-1.1.2.jar
+ * XPath常用API:
+ * List<Node> selectNodes(String var1):检索出一批节点集合。
+ * Node selectSingleNode(String var1)：检索出一个节点返回。
+ * XPath提供的四种检索数据的写法：
+ * 1.绝对路径。
+ * 2.相对路径。
+ * 3.全文搜索。
+ * 4.属性查找。
+ * 小结：
+ * 1.绝对路径： /根元素/子元素/子元素。
+ * 2.相对路径： ./子元素/子元素。 (.代表了当前元素)
+ * 3.全文搜索：
+ * //元素  在全文找这个元素
+ * //元素1/元素2  在全文找元素1下面的一级元素2
+ * //元素1//元素2  在全文找元素1下面的全部元素2
+ * 4.属性查找。
+ * //@属性名称  在全文检索属性对象。
+ * //元素[@属性名称]  在全文检索包含该属性的元素对象。
+ * //元素[@属性名称=值]  在全文检索包含该属性的元素且属性值为该值的元素对象。
  */
 public class XPathDemo {
     /**
-     1.绝对路径: /根元素/子元素/子元素。
+     * 1.绝对路径: /根元素/子元素/子元素。
      */
     @Test
     public void parse01() throws Exception {
@@ -52,13 +53,13 @@ public class XPathDemo {
         // c、检索全部的名称
         List<Node> nameNodes = document.selectNodes("/contactList/contact/name");
         for (Node nameNode : nameNodes) {
-            Element  nameEle = (Element) nameNode;
+            Element nameEle = (Element) nameNode;
             System.out.println(nameEle.getTextTrim());
         }
     }
 
     /**
-     2.相对路径： ./子元素/子元素。 (.代表了当前元素)
+     * 2.相对路径： ./子元素/子元素。 (.代表了当前元素)
      */
     @Test
     public void parse02() throws Exception {
@@ -71,16 +72,16 @@ public class XPathDemo {
         // c、检索全部的名称
         List<Node> nameNodes = root.selectNodes("./contact/name");
         for (Node nameNode : nameNodes) {
-            Element  nameEle = (Element) nameNode;
+            Element nameEle = (Element) nameNode;
             System.out.println(nameEle.getTextTrim());
         }
     }
 
     /**
-     3.全文搜索：
-     //元素  在全文找这个元素
-     //元素1/元素2  在全文找元素1下面的一级元素2
-     //元素1//元素2  在全文找元素1下面的全部元素2
+     * 3.全文搜索：
+     * //元素  在全文找这个元素
+     * //元素1/元素2  在全文找元素1下面的一级元素2
+     * //元素1//元素2  在全文找元素1下面的全部元素2
      */
     @Test
     public void parse03() throws Exception {
@@ -94,16 +95,16 @@ public class XPathDemo {
         // List<Node> nameNodes = document.selectNodes("//contact/name");
         List<Node> nameNodes = document.selectNodes("//contact//name");
         for (Node nameNode : nameNodes) {
-            Element  nameEle = (Element) nameNode;
+            Element nameEle = (Element) nameNode;
             System.out.println(nameEle.getTextTrim());
         }
     }
 
     /**
-     4.属性查找。
-     //@属性名称  在全文检索属性对象。
-     //元素[@属性名称]  在全文检索包含该属性的元素对象。
-     //元素[@属性名称=值]  在全文检索包含该属性的元素且属性值为该值的元素对象。
+     * 4.属性查找。
+     * //@属性名称  在全文检索属性对象。
+     * //元素[@属性名称]  在全文检索包含该属性的元素对象。
+     * //元素[@属性名称=值]  在全文检索包含该属性的元素且属性值为该值的元素对象。
      */
     @Test
     public void parse04() throws Exception {

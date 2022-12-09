443d38ced994a01233a4b6e8ba12901fb5caa3da
xyz <xieyinzhangl@gmail.com>
2022-12-09 19:47:11 +0800

    first commit

--- xml-app/src/com/xyz/d4_decorator_pattern/DecoratorPattern.java
+++ xml-app/src/com/xyz/d4_decorator_pattern/DecoratorPattern.java
@@ -4,7 +4,6 @@ public class DecoratorPattern {
     public static void main(String[] args) {
         InputStream is = new BufferInputStream(new FileInputStream());
         System.out.println(is.read());
-
         System.out.println(is.read(new byte[3]));
     }
 }
